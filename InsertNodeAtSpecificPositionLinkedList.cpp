5b7053d4e061809e1161836093cb13095c3af80c
aydogduwu <aydogdu.mustafa@metu.edu.tr>
2022-03-25 23:31:57 +0300

    Solution for Insert a node at a specific position in a linked list.

--- InsertNodeAtSpecificPositionLinkedList.cpp
+++ InsertNodeAtSpecificPositionLinkedList.cpp
@@ -101,3 +101,39 @@ SinglyLinkedListNode* insertNodeAtPosition(SinglyLinkedListNode* llist, int data
 }
 
 int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    SinglyLinkedList* llist = new SinglyLinkedList();
+
+    int llist_count;
+    cin >> llist_count;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    for (int i = 0; i < llist_count; i++) {
+        int llist_item;
+        cin >> llist_item;
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+        llist->insert_node(llist_item);
+    }
+
+    int data;
+    cin >> data;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    int position;
+    cin >> position;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    SinglyLinkedListNode* llist_head = insertNodeAtPosition(llist->head, data, position);
+
+    print_singly_linked_list(llist_head, " ", fout);
+    fout << "\n";
+
+    free_singly_linked_list(llist_head);
+
+    fout.close();
+
+    return 0;
+}
